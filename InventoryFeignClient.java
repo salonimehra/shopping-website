7776e872375633eb13995d4bf14f6a38b698dc4f
up2612 <52831251+up2612@users.noreply.github.com>
2021-09-14 17:04:24 +0530

    Updates in placed-order for integration with inventory

---
+++ InventoryFeignClient.java
@@ -0,0 +1,15 @@
+package com.cts.placed_orders.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cts.placed_orders.model.ProductDetails;
+
+@FeignClient(name="inventory",url="${inventory.url}")
+public interface InventoryFeignClient {
+
+	@GetMapping("/products/{id}")
+	public ProductDetails getProductById(@PathVariable("id") long id);
+}
---
+++ PlacedOrderController.java
@@ -0,0 +1,89 @@
+package com.cts.placed_orders.controllers;
+
+import java.util.List;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.placed_orders.feign.AuthenticationFeignClient;
+import com.cts.placed_orders.model.OrderDetails;
+import com.cts.placed_orders.model.ProductDetails;
+import com.cts.placed_orders.service.PlacedOrdersService;
+
+@RestController
+@RequestMapping(value="/api/v1")
+public class PlacedOrderController {
+
+	@Autowired
+	PlacedOrdersService placedOrdersService;
+	
+	@Autowired
+	AuthenticationFeignClient authenticationfeign;
+	
+	@PostMapping("/placeorder/{productId}/{productQuantity}")
+	public ResponseEntity<?> placeorder(@PathVariable Integer productId, @PathVariable Integer  productQuantity,
+			@RequestHeader(value = "Authorization",required =  true) String requestTokenHeader){
+		
+		if (authenticationfeign.authorizeTheRequest(requestTokenHeader))
+		{
+			Long orderedProductId=new Long(productId);
+			// Integer productQuantity =2;
+				
+			System.out.println(productId+" controller"+productQuantity);
+		boolean status=placedOrdersService.placeorder(orderedProductId,productQuantity);
+		//later appropriate exception to be added
+		if(status == true)
+		return new ResponseEntity<>(status,HttpStatus.OK);
+		else {
+			return new ResponseEntity<>("Out of stock",HttpStatus.NOT_FOUND);
+
+		}
+		}
+		else {
+			//later add proper exception
+			return new ResponseEntity<>("Out of stock",HttpStatus.PRECONDITION_FAILED);
+
+		}
+	}
+	
+	@PostMapping("/getordersbyid")
+	public ResponseEntity<?> getOrdersByid(@RequestBody Integer id,
+			@RequestHeader(value = "Authorization",required =  true) String requestTokenHeader){
+		if (authenticationfeign.authorizeTheRequest(requestTokenHeader))
+		{
+		OrderDetails orderDetails=placedOrdersService.getOrderDetailsById(id);
+		if(Objects.isNull(orderDetails)) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		else return new ResponseEntity<>(orderDetails,HttpStatus.OK);
+		}
+		else {
+			//later add proper exception
+			return new ResponseEntity<>("Out of stock",HttpStatus.PRECONDITION_FAILED);
+
+		}
+	}
+	
+	@GetMapping("/getorders")
+	public ResponseEntity<?> getOrders(@RequestHeader(value = "Authorization",required =  true) String requestTokenHeader){
+		if (authenticationfeign.authorizeTheRequest(requestTokenHeader))
+		{
+		List<OrderDetails> orderDetails=placedOrdersService.getPlacedOrdersDetail();
+		if(Objects.isNull(orderDetails.get(0))) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		else return new ResponseEntity<>(orderDetails,HttpStatus.OK);
+		}
+		else {
+			//later add proper exception
+			return new ResponseEntity<>("Out of stock",HttpStatus.PRECONDITION_FAILED);
+
+		}
+	}
+}
