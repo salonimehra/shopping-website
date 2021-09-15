b12ef8c1dafcee77087960d310c4e9fee991217e
up2612 <52831251+up2612@users.noreply.github.com>
2021-09-15 14:15:32 +0530

    Changes in usernames and passwords

--- Authorizatiion-Microservice/src/main/resources/data.sql
+++ Authorizatiion-Microservice/src/main/resources/data.sql
@@ -1,5 +1,6 @@
 insert into users (id, password, user_name) values (1, 'password@admin', 'admin');
 insert into users (id, password, user_name) values (2, 'password@pandey', 'Uddeshay Pandey');
-insert into users (id, password, user_name) values (3, 'password@monika', 'Monika');
-insert into users (id, password, user_name) values (4, 'password@manikanta', 'Manikanta');
-insert into users (id, password, user_name) values (5, 'password@chiranjeevi', 'Chiranjeevi');
+insert into users (id, password, user_name) values (3, 'password@kashaf', 'Kashaf');
+insert into users (id, password, user_name) values (4, 'password@saloni', 'Saloni');
+insert into users (id, password, user_name) values (5, 'password@prince', 'Prince');
+insert into users (id, password, user_name) values (6, 'password@daksha', 'daksha');
