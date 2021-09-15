bb281190419420ccd738124c4a8567a3f7a41e07
up2612 <52831251+up2612@users.noreply.github.com>
2021-09-15 14:03:59 +0530

    Bug fixation
    Bug fixation for integration with inventory

--- placed_orders/src/main/java/com/cts/placed_orders/controllers/PlacedOrderController.java
+++ placed_orders/src/main/java/com/cts/placed_orders/controllers/PlacedOrderController.java
@@ -17,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cts.placed_orders.feign.AuthenticationFeignClient;
 import com.cts.placed_orders.model.OrderDetails;
-import com.cts.placed_orders.model.ProductDetails;
+//import com.cts.placed_orders.model.ProductDetails;
 import com.cts.placed_orders.service.PlacedOrdersService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping(value="/api/v1")
+@Slf4j
 public class PlacedOrderController {
 
 	@Autowired
@@ -30,59 +33,94 @@ public class PlacedOrderController {
 	@Autowired
 	AuthenticationFeignClient authenticationfeign;
 	
+	/*
+	 *  Method Name --> placeorder
+	 *  @param      --> productId and Product Quantity
+	 *  @return     --> String
+	 *  This method takes product id and product quantity and checks whether enough stock is
+	 *  there or not. If stock is there it will place the order else it will throw out of stock
+	 *  exception 
+	 */
 	@PostMapping("/placeorder/{productId}/{productQuantity}")
 	public ResponseEntity<?> placeorder(@PathVariable Integer productId, @PathVariable Integer  productQuantity,
 			@RequestHeader(value = "Authorization",required =  true) String requestTokenHeader){
 		
+		log.info("place order entry point started of placed-order microservice");
 		if (authenticationfeign.authorizeTheRequest(requestTokenHeader))
 		{
-			Long orderedProductId=new Long(productId);
-			// Integer productQuantity =2;
-				
-			System.out.println(productId+" controller"+productQuantity);
-		boolean status=placedOrdersService.placeorder(orderedProductId,productQuantity);
-		//later appropriate exception to be added
-		if(status == true)
-		return new ResponseEntity<>(status,HttpStatus.OK);
-		else {
-			return new ResponseEntity<>("Out of stock",HttpStatus.NOT_FOUND);
-
-		}
+		
+			Long orderedProductId=new Long(productId);		
+			boolean status=placedOrdersService.placeorder(orderedProductId,productQuantity);
+			//later appropriate exception to be added
+			if(status == true) {
+				log.info("Returning placed order true status from place order entry point");
+				return new ResponseEntity<>(status,HttpStatus.OK);
+			}
+			else {
+				log.info("Returning out of stock exception from place order entry point");
+				return new ResponseEntity<>("Out of stock",HttpStatus.NOT_FOUND);
+			}
 		}
 		else {
 			//later add proper exception
+			log.info("Authentication failed for placed order entry point");
 			return new ResponseEntity<>("Out of stock",HttpStatus.PRECONDITION_FAILED);
 
 		}
 	}
 	
+	/*
+	 *  Method Name --> getOrdersByid
+	 *  @param      --> orderId
+	 *  @return     --> Order details
+	 *  This method takes order id as parameter and search for the order. If that order is present
+	 *  it returns the order details else return order not found exception
+	 */
 	@PostMapping("/getordersbyid")
-	public ResponseEntity<?> getOrdersByid(@RequestBody Integer id,
+	public ResponseEntity<?> getOrdersByid(@RequestBody Integer orderId,
 			@RequestHeader(value = "Authorization",required =  true) String requestTokenHeader){
+		log.info("get order by id entry point started of placed-order microservice");
 		if (authenticationfeign.authorizeTheRequest(requestTokenHeader))
 		{
-		OrderDetails orderDetails=placedOrdersService.getOrderDetailsById(id);
-		if(Objects.isNull(orderDetails)) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		else return new ResponseEntity<>(orderDetails,HttpStatus.OK);
+		OrderDetails orderDetails=placedOrdersService.getOrderDetailsById(orderId);
+		if(Objects.isNull(orderDetails)) {
+			log.info("Returning resultant data not found from get order by id entry point");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();}
+		else {
+			log.info("Returning resultant data from get order by id entry point");
+			return new ResponseEntity<>(orderDetails,HttpStatus.OK);}
 		}
 		else {
+			log.info("Authentication failed for get order by id entry point");
 			//later add proper exception
-			return new ResponseEntity<>("Out of stock",HttpStatus.PRECONDITION_FAILED);
+			return new ResponseEntity<>("Authentication failed",HttpStatus.PRECONDITION_FAILED);
 
 		}
 	}
 	
-	@GetMapping("/getorders")
-	public ResponseEntity<?> getOrders(@RequestHeader(value = "Authorization",required =  true) String requestTokenHeader){
+	/*
+	 *  Method Name --> getOrders
+	 *  @return     --> Order details
+	 *  This method returns the details of all the orders placed.
+	 */
+	@GetMapping("/getallorders")
+	public ResponseEntity<?> getAllOrders(@RequestHeader(value = "Authorization",required =  true) String requestTokenHeader){
+		log.info("get all orders entry point started of placed-order microservice");
 		if (authenticationfeign.authorizeTheRequest(requestTokenHeader))
 		{
 		List<OrderDetails> orderDetails=placedOrdersService.getPlacedOrdersDetail();
-		if(Objects.isNull(orderDetails.get(0))) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		else return new ResponseEntity<>(orderDetails,HttpStatus.OK);
+		if(Objects.isNull(orderDetails.get(0))) {
+			log.info("Returning resultant data not found from get all orders entry point");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		else {
+			log.info("Returning resultant data from get all orders entry point");
+			return new ResponseEntity<>(orderDetails,HttpStatus.OK);}
 		}
 		else {
 			//later add proper exception
-			return new ResponseEntity<>("Out of stock",HttpStatus.PRECONDITION_FAILED);
+			log.info("Authentication failed for get all orders entry point");
+			return new ResponseEntity<>("Authentication failed",HttpStatus.PRECONDITION_FAILED);
 
 		}
 	}
