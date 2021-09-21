0b2eb6ea55e8e0ea88d80a95dda1268a102c6195
up2612 <52831251+up2612@users.noreply.github.com>
2021-09-21 18:14:31 +0530

    Integration and fixing of bugs
    added @entity in StockDetails class to remove error table schema not present.

--- Inventory-Management/src/main/java/com/Inventory/Model/StockDetails.java
+++ Inventory-Management/src/main/java/com/Inventory/Model/StockDetails.java
@@ -1,14 +1,18 @@
 package com.Inventory.Model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Table(name="StockDetails")
+@Entity
 public class StockDetails {
 	
+	@Id
 	@Column(name="productId")
 	private long id;
 	
