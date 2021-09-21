1e5cf0392f6ab1dcdd1d2738d1cfaeb6ec8a46a0
up2612 <52831251+up2612@users.noreply.github.com>
2021-09-21 18:10:30 +0530

    Integration and fixing of bugs
    added @enablefiegnclient annotation for enabling feign client calls

--- Inventory/src/main/java/com/shopping/product/InventoryApplication.java
+++ Inventory/src/main/java/com/shopping/product/InventoryApplication.java
@@ -3,9 +3,11 @@ package com.shopping.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableFeignClients
 public class InventoryApplication {
 
 	public static void main(String[] args) {
